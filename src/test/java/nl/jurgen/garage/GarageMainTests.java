094650bdfd0bc9e310ff97565ae6448f16214079
jurgenkervezee <jurgenbla@gmail.com>
2020-12-24 10:18:08 +0100

    Added the rest

---
+++ src/test/java/nl/jurgen/garage/GarageMainTests.java
@@ -0,0 +1,13 @@
+package nl.jurgen.garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
